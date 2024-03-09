43ff774588ca498fe54dad1a751cba921ff637ab
siljamat <siljama@hotmail.fi>
2024-03-09 15:25:46 +0200

    fetches coordinates correctly now

--- backend/src/functions/eventApiFetch.ts
+++ backend/src/functions/eventApiFetch.ts
@@ -5,26 +5,31 @@ import fetchData from './fetchData';
 const eventApiFetch = async (url: string, options: RequestInit = {}) => {
   const data: any[] = await fetchData(url, options);
   if (data) {
-    const apiEvents: Partial<Event>[] = (data as any).data.map((event: any) => {
-      return {
-        id: event.id,
-        created_at: event.created_time,
-        event_name: event.name.fi,
-        description: event.description.fi,
-        date: event.start_time,
-        location: event.location,
-        email: '',
-        organizer: event.publisher,
-        address: '',
-        age_restrictions: '',
-        event_site: event.info_url,
-        ticket_site: '',
-        price: '',
-        image: event.images[0],
-        audience_min_age: event.audience_min_age,
-        audience_max_age: event.audience_max_age,
-      };
-    });
+    const apiEvents: Partial<Event>[] = await Promise.all(
+      (data as any).data.map(async (event: any) => {
+        // Fetch the location details
+        const locationData: any = await fetchData(event.location['@id']);
+
+        return {
+          id: event.id,
+          created_at: event.created_time,
+          event_name: event.name.fi,
+          description: event.description.fi,
+          date: event.start_time,
+          location: locationData.position,
+          email: '',
+          organizer: event.publisher,
+          address: '',
+          age_restrictions: '',
+          event_site: event.info_url,
+          ticket_site: '',
+          price: '',
+          image: event.images[0],
+          audience_min_age: event.audience_min_age,
+          audience_max_age: event.audience_max_age,
+        };
+      }),
+    );
     return apiEvents;
   }
   throw new Error('No data');
